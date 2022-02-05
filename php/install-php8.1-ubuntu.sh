eacaf70d9c0ae57d0ad7a7927f9214275aef362e
Vinicius Melo <viniciusldemelo@gmail.com>
2022-02-04 21:03:39 -0300

    alterações

---
+++ php/install-php8.1-ubuntu.sh
@@ -0,0 +1,6 @@
+// Enable PPA for PHP 8.1 in your system and install it.
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+
+sudo apt-get install php8.1
+sudo apt-get install php8.1-cli php8.1-common php8.1-opcache php8.1-mysql php8.1-mbstring php8.1-zip php8.1-fpm php8.1-intl php8.1-xml
---
+++ php/php8.1_to_php7.4.sh
@@ -0,0 +1,15 @@
+// a2dismod disables the php8.0 module by removing those symlinks.
+sudo a2dismod php8.1
+
+// a2enmod enables php7.4 module within the apache2 configuration.
+sudo a2enmod php7.4
+
+// Restart apache2 service.
+sudo service apache2 restart
+
+// Set alternative name path.
+sudo update-alternatives --set php /usr/bin/php7.4
+sudo update-alternatives --set phar /usr/bin/phar7.4
+sudo update-alternatives --set phar.phar /usr/bin/phar.phar7.4
+sudo update-alternatives --set phpize /usr/bin/phpize7.4
+sudo update-alternatives --set php-config /usr/bin/php-config7.4
