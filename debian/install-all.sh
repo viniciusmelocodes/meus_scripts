76c956545652d9a6efd3d3b3f4b7e06f9280e98b
Vinícius Melo <viniciusldemelo@gmail.com>
2024-06-11 15:12:19 -0300

    Alteração para rodar todas instalações automaticamente.

--- debian/install-all.sh
+++ debian/install-all.sh
@@ -5,25 +5,25 @@ apt install -y git wget curl xclip docker docker-compose flatpak gnome-software-
 apt install -f
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flathub com.github.IsmaelMartinez.teams_for_linux
-flatpak install flathub com.visualstudio.code
-flatpak install flathub io.dbeaver.DBeaverCommunity
-flatpak install flathub it.fabiodistasio.AntaresSQL
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub com.gitfiend.GitFiend
-flatpak install flathub com.google.Chrome
-flatpak install flathub org.telegram.desktop
-flatpak install flathub com.anydesk.Anydesk
-flatpak install flathub rest.insomnia.Insomnia
-flatpak install flathub com.slack.Slack
-flatpak install flathub org.remmina.Remmina
-flatpak install flathub org.filezillaproject.Filezilla
-flatpak install flathub com.wps.Office
-flatpak install flathub io.github.jorchube.monitorets
-flatpak install flathub com.microsoft.Edge
-flatpak install flathub org.videolan.VLC
-flatpak install flathub io.github.mahmoudbahaa.outlook_for_linux
-flatpak install flathub org.kde.isoimagewriter
+flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
+flatpak install -y flathub com.visualstudio.code
+flatpak install -y flathub io.dbeaver.DBeaverCommunity
+flatpak install -y flathub it.fabiodistasio.AntaresSQL
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub com.gitfiend.GitFiend
+flatpak install -y flathub com.google.Chrome
+flatpak install -y flathub org.telegram.desktop
+flatpak install -y flathub com.anydesk.Anydesk
+flatpak install -y flathub rest.insomnia.Insomnia
+flatpak install -y flathub com.slack.Slack
+flatpak install -y flathub org.remmina.Remmina
+flatpak install -y flathub org.filezillaproject.Filezilla
+flatpak install -y flathub com.wps.Office
+flatpak install -y flathub io.github.jorchube.monitorets
+flatpak install -y flathub com.microsoft.Edge
+flatpak install -y flathub org.videolan.VLC
+flatpak install -y flathub io.github.mahmoudbahaa.outlook_for_linux
+flatpak install -y flathub org.kde.isoimagewriter
 
 cd ~
 mkdir .ssh
