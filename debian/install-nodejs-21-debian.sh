e28478da2fcb50288319758404656b963631e3f4
Vinícius Melo <vinicius.simplicio@artit.com.br>
2024-06-16 23:31:45 -0300

    Atualização script node 21

--- debian/install-nodejs-21-debian.sh
+++ debian/install-nodejs-21-debian.sh
@@ -1,7 +1,7 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.1/install.sh | bash
 source ~/.bashrc
 nvm --version
-nvm install 20
+nvm install 21
 nvm alias default node
 node -v
 nvm ls
